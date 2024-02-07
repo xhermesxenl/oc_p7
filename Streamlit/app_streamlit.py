02a31fa5b008c75f9c93af375bdf06839832f509
a <a>
2024-02-07 23:40:25 +0100

    --feat ajout du graphique du score

--- Streamlit/app_streamlit.py
+++ Streamlit/app_streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import matplotlib.pyplot as plt
 
 # Définissez le titre de l'application
 st.title("Interface Streamlit pour l'API de prédiction de crédit")
@@ -10,8 +11,9 @@ client_id = st.text_input("Entrez l'ID du client (ex: 124782):")
 # Créez un bouton pour interroger l'API
 if st.button("Obtenir la prédiction"):
     if client_id:
-        # Faites la requête à l'API Flask en local
-        api_url = f"http://127.0.0.1:5000/credit/{client_id}/predict"
+        # Requête à l'API Flask
+        api_url = f"https://ocp7-dc846df71c5b.herokuapp.com/api/predict/{client_id}"
+
         response = requests.get(api_url)
 
         if response.status_code == 200:
@@ -19,6 +21,19 @@ if st.button("Obtenir la prédiction"):
             st.write("Résultat de la prédiction :")
             st.write(f"Probabilité : {data['probability']}%")
             st.write(f"Classe : {data['classe']}")
+
+            st.bar_chart({"Accepté": data['probability'], "Refusé": 100 - data['probability']})
+
+            st.text(f"Compteur : {data['probability']}")
+
+
+            labels = ['accepte', 'refuse']
+            colors = ['green' if data['classe'] == 'accepte' else 'red']
+            plt.pie([data['probability'], 100 - data['probability']], labels=labels, colors=colors, autopct='%1.1f%%')
+            plt.legend(labels)
+            st.pyplot(plt)
+
+
         elif response.status_code == 404:
             st.write("ID inconnu.")
         else:
